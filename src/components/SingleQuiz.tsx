6791acfe75479e170df0133f4e69387fe12b82f5
Joo <vijak13019@hotmail.com>
2022-12-28 11:58:25 +0700

    feat: add icons indicate correct or wrong when submit

--- src/components/SingleQuiz.tsx
+++ src/components/SingleQuiz.tsx
@@ -1,6 +1,8 @@
 import { Quiz } from '../models/model';
 import ChoiceButton from './ChoiceButton';
 import { useEffect } from 'react';
+import { BsCheckLg } from 'react-icons/bs';
+import { ImCross } from 'react-icons/im';
 
 interface Props {
   quiz: Quiz;
@@ -22,8 +24,17 @@ const SingleQuiz: React.FC<Props> = ({
 
   return (
     <div className="mt-8 text-dark-violet">
+      {isEnded && (
+        <>
+          {quiz.choose === quiz.correct_answer ? (
+            <BsCheckLg className="mr-12 inline-block text-green-500" />
+          ) : (
+            <ImCross className="mr-12 inline-block text-red-500" />
+          )}
+        </>
+      )}
       <h1
-        className="text-xl font-bold"
+        className="inline-block text-xl font-bold"
         dangerouslySetInnerHTML={{ __html: quiz.question }}
       />
       <div className="flex justify-around">
